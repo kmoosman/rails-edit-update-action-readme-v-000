00a602c246a4702c8f91153649a825ed9e4accd5
Katie Moosman <moosman.katie@gmail.com>
2019-04-02 04:37:49 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :articles, only: [:index, :show, :new, :create, :edit, :update]
 
 
-  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
-  patch 'articles/:id', to: 'articles#update'
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch 'articles/:id', to: 'articles#update'
 end
