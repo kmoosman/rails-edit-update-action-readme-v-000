e8676da287e024879430c01ff89c5560ce9d0dd9
Katie Moosman <moosman.katie@gmail.com>
2019-04-02 04:54:25 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,7 +27,6 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(title: params[:title], description: params[:description])
-    @article.save
     redirect_to article_path(@article)
   end
 end
